b9c2e436bb9ba95f236881a8e9ad272e1dce6d02
Edgars Skrabiņš <shiftkiro@gmail.com>
2026-08-31 17:55:29 +0300

    Test code

--- Assets/Scripts/TurnOwnerTest.cs
+++ Assets/Scripts/TurnOwnerTest.cs
@@ -1,11 +1,15 @@
+using Photon.Pun;
 using TMPro;
 using UnityEngine;
 
 public class TurnOwnerTest : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI m_text;
+    [SerializeField] private TextMeshProUGUI m_turnOwnerIdText;
+    [SerializeField] private TextMeshProUGUI m_turnOwnerNameText;
     void Update()
     {
-        m_text.text = TurnManager.I.GetTurnOwnerViewId().ToString();
-    }
+        int turnOwnerId = TurnManager.I.GetTurnOwnerViewId();
+        m_turnOwnerIdText.text = "Current turn owner id: " + turnOwnerId;
+        // m_turnOwnerNameText.text = "Turn owner name: " + PhotonView.Find(turnOwnerId)?.Owner?.NickName;
+     }
 }
