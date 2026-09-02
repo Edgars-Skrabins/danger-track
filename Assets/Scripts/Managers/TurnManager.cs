58fb24df674192a7f5a74f8ee7fffca434f39582
Edgars Skrabiņš <shiftkiro@gmail.com>
2026-09-02 17:38:21 +0300

    Removed end turn trying log

--- Assets/Scripts/Managers/TurnManager.cs
+++ Assets/Scripts/Managers/TurnManager.cs
@@ -45,7 +45,6 @@ public partial class TurnManager : NetworkedSingleton<TurnManager>
 
     public void EndTurn()
     {
-        Debug.Log("Trying to end turn");
         if (!PhotonNetwork.IsMasterClient) return;
         Debug.Log("Ending turn");
         StartNextTurn();
